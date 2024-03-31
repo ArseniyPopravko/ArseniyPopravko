983f471877934edbedd50f6901ebb4c0df75413b
bot1nokk <159694695+bot1nokk@users.noreply.github.com>
2024-03-31 19:46:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let question = document.querySelectorAll('.quest')
 let answer_list = document.querySelectorAll('.ans')
-
+let bu1 = document.querySelector('.button')
+let answer_input = document.querySelectorAll('.answer_input')
 
 class Question{
     constructor(QUESTION, a1, a2, aright){
@@ -24,9 +25,12 @@ function display(q, i){
     console.log(q, i)
         question[i].innerHTML = q.QUESTION
         i = i * 3
-        answer_list[i].innerHTML = q.a1
-        answer_list[i+1].innerHTML = q.a2
-        answer_list[i+2].innerHTML = q.aright
+        answer_list[i].innerHTML += q.a1
+        answer_input[i].value = q.a1
+        answer_list[i+1].innerHTML += q.a2
+        answer_input[i+1].value = q.a2
+        answer_list[i+2].innerHTML += q.aright
+        answer_input[i+2].value = q.aright
 
 }
 
@@ -45,6 +49,26 @@ answers = [
 //let total_answer = 0
 //let cur_question = QUESTION[total_answer]
 //cur_question.display()
+
+true_answers = []
+corect_answer = 0
+
+bu1.addEventListener('click', function() {
+    let answer_input_1 = document.querySelectorAll('.answer_input')
+    console.log(answer_input)
+    for (let i = 0; i < answer_input.length; i++){
+        if (answer_input_1[i].checked){
+            true_answers.push(answer_input[i].value)
+        }
+    }
+    for (let i = 0; i < true_answers.length; i++){
+        if (true_answers[i] == questions[i].aright){
+            corect_answer += 1
+        }
+    }
+    alert(corect_answer)
+    true_answers = []
+})
 
 
 
