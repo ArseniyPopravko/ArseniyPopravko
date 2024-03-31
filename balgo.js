d94340f6f08f18e74a4ad3b795c33231fdd03845
bot1nokk <159694695+bot1nokk@users.noreply.github.com>
2024-03-31 18:07:45 +0700

    balgo.js

---
+++ balgo.js
@@ -0,0 +1,16 @@
+let but = document.querySelector('.button')
+let but1 = document.querySelector('.button1')
+let body = document.querySelector()
+
+but1.addEventListener('click', function() {
+   if (but.value == '1'){
+      window.location.href = ""
+   }
+   else if (but.value == '2'){
+      window.location.href = ""
+   }
+   else if (but.value == '3'){
+      window.location.href = ""
+   }
+   
+})
